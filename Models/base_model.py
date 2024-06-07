a4b3d8fd8495e78b8190ea04da2d1ba014ee4dea
ghofranaaa <smailikhaled841@gmail.com>
2024-06-07 12:30:42 +0100

    commit

--- Models/base_model.py
+++ Models/base_model.py
@@ -6,6 +6,7 @@ from datetime import datetime
 class BaseModel:
     def __init__(self):
         self.id = str(uuid.uuid4())
+        self.create = create()
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
 
