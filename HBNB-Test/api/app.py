4887bd682e8b3265e3fbce7db8ab403fe832454c
oussematurki0 <oussematurki0@gmail.com>
2024-06-11 15:54:44 +0200

    APP.py modified

--- HBNB-Test/api/app.py
+++ HBNB-Test/api/app.py
@@ -2,74 +2,90 @@
 from flask import Flask, jsonify, request
 from models.user import User
 from models.place import Place
-from models.review import Review
-from models.amenity import Amenity
-from models.city import City
-from models.country import Country
 from persistence.data_manager import DataManager
-from models.user import User
-import sys
-
-# Append the models directory to the system path
-sys.path.append('/home/oussema/HBNB-Test/models')
 
 # Create a Flask application
 app = Flask(__name__)
 # Create a DataManager instance
 data_manager = DataManager()
-
+#-------------------------------------------------------------------------------------------------------------------------
 # Route to create a new user
 @app.route('/users', methods=['POST'])
 def create_user():
-    # Get the JSON data from the request
     data = request.get_json()
-    # Create a new User instance with the data
-    user = User(email=data['email'], first_name=data['first_name'], last_name=data['last_name'])
-    # Save the user to the data manager
-    data_manager.save(user)
-    # Return the user data as JSON with a status code of 201 (created)
-    return jsonify(user.to_dict()), 201
+    if not all(key in data for key in ['email', 'first_name', 'last_name']):
+        return jsonify({'error': 'Missing data'}), 400
 
+    user = User(email=data['email'], first_name=data['first_name'], last_name=data['last_name'])
+    data_manager.save(user)
+    return jsonify(user.to_dict()), 201
+#-------------------------------------------------------------------------------------------------------------------------
 # Route to get all users
 @app.route('/users', methods=['GET'])
 def get_users():
-    # Get all user IDs from the data manager and fetch each user
     users = [data_manager.get(user_id, 'User') for user_id in data_manager.data['User']]
-    # Return the list of users as JSON with a status code of 200 (OK)
     return jsonify(users), 200
-
+#-------------------------------------------------------------------------------------------------------------------------
 # Route to get a specific user by ID
 @app.route('/users/<user_id>', methods=['GET'])
 def get_user(user_id):
-    # Get the user with the given ID from the data manager
     user = data_manager.get(user_id, 'User')
-    # If the user exists, return it as JSON with a status code of 200 (OK)
     if user:
         return jsonify(user), 200
-    # If the user does not exist, return an error message as JSON with a status code of 404 (Not Found)
     else:
         return jsonify({'error': 'User not found'}), 404
-
+#-------------------------------------------------------------------------------------------------------------------------
 # Route to update a user by ID
 @app.route('/users/<user_id>', methods=['PUT'])
 def update_user(user_id):
-    # Get the JSON data from the request
     data = request.get_json()
-    # Create a new User instance with the updated data and the same ID
     user = User(email=data['email'], first_name=data['first_name'], last_name=data['last_name'], id=user_id)
-    # Update the user in the data manager
     data_manager.update(user)
-    # Return the updated user data as JSON with a status code of 200 (OK)
     return jsonify(user.to_dict()), 200
-
+#-------------------------------------------------------------------------------------------------------------------------
 # Route to delete a user by ID
 @app.route('/users/<user_id>', methods=['DELETE'])
 def delete_user(user_id):
-    # Delete the user with the given ID from the data manager
     data_manager.delete(user_id, 'User')
-    # Return an empty response with a status code of 204 (No Content)
     return '', 204
-
+#-------------------------------------------------------------------------------------------------------------------------
+# POST /places
+@app.route('/places', methods=['POST'])
+def create_place():
+    data = request.get_json()
+    place = Place(name=data['name'], description=data['description'], ...)
+    data_manager.save(place)
+    return jsonify(place.to_dict()), 201
+#-------------------------------------------------------------------------------------------------------------------------
+# GET /places
+@app.route('/places', methods=['GET'])
+def get_places():
+    places = [data_manager.get(place_id, 'Place') for place_id in data_manager.data['Place']]
+    return jsonify(places), 200
+#-------------------------------------------------------------------------------------------------------------------------
+# GET /places/{place_id}
+@app.route('/places/<place_id>', methods=['GET'])
+def get_place(place_id):
+    place = data_manager.get(place_id, 'Place')
+    if place:
+        return jsonify(place), 200
+    else:
+        return jsonify({'error': 'Place not found'}), 404
+#-------------------------------------------------------------------------------------------------------------------------
+# PUT /places/{place_id}
+@app.route('/places/<place_id>', methods=['PUT'])
+def update_place(place_id):
+    data = request.get_json()
+    place = Place(name=data['name'], description=data['description'], ...)
+    data_manager.update(place)
+    return jsonify(place.to_dict()), 200
+#-------------------------------------------------------------------------------------------------------------------------
+# DELETE /places/{place_id}
+@app.route('/places/<place_id>', methods=['DELETE'])
+def delete_place(place_id):
+    data_manager.delete(place_id, 'Place')
+    return '', 204
+#-------------------------------------------------------------------------------------------------------------------------
 # Run the Flask application if this script is executed directly
 if __name__ == '__main__':
     app.run(debug=True)
