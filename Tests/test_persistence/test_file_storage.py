a960fd84121ff69670966ed8c5da030ccc3539cf
oussematurki0 <oussematurki0@gmail.com>
2024-06-18 19:24:37 +0200

    deleted other files

--- Tests/test_persistence/test_file_storage.py
+++
@@ -1,3 +0,0 @@
-import sys, os
-current_path = os.path.abspath(os.path.dirname(__file__))
-sys.path.append(os.path.join(current_path, '..'))
--- app.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask
-from api.views.users import app as users_app
-
-app = Flask(__name__)
-app.register_blueprint(users_app, url_prefix='/api/v1')
-
-if __name__ == '__main__':
-    app.run(debug=True)
